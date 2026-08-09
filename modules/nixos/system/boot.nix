b83c73ff9113824a03c5cdfeb24608701e91ddd5
Elias Schlosser <eljangus@gmail.com>
2026-08-09 14:36:04 +0200

    kernel 7.1.5 pin

--- modules/nixos/system/boot.nix
+++ modules/nixos/system/boot.nix
@@ -8,7 +8,16 @@
     boot = {
       loader.systemd-boot.enable = true;
       loader.efi.canTouchEfiVariables = true;
-      kernelPackages = pkgs.linuxPackages_latest;
+      kernelPackages = pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_7_1.override {
+        argsOverride = rec {
+          src = pkgs.fetchurl {
+            url = "mirror://kernel/linux/kernel/v${lib.versions.major version}.x/linux-${version}.tar.xz";
+            sha256 = "sha256-IqAZazy83zTcJ7d1YfTQQFhf00R+3JqzUxoax54wQec=";
+          };
+          version = "7.1.5";
+          modDirVersion = "7.1.5";
+        };
+      });
     };
   };
 }
