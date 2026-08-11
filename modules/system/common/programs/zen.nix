622f07241ae374d45b396526ac5c47bc734db81b
Elias Schlosser <eljangus@gmail.com>
2026-08-11 23:16:31 +0200

    updated zen.nix

--- modules/system/common/programs/zen.nix
+++ modules/system/common/programs/zen.nix
@@ -5,14 +5,37 @@
   pkgs,
   ...
 }: let
-  zen = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.beta.override {
-    # https://mozilla.github.io/policy-templates/
-    policies = {
+  zenPackages = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system};
+
+  prefs = {
+    "extensions.autoDisableScopes" = 0;
+    "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+    "devtools.chrome.enabled" = true;
+    "browser.aboutConfig.showWarning" = false;
+    "browser.shell.checkDefaultBrowser" = false;
+    "browser.newtabpage.activity-stream.showSponsored" = false;
+    "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+    "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+    "datareporting.healthreport.uploadEnabled" = false;
+    "toolkit.telemetry.enabled" = false;
+    "toolkit.telemetry.unified" = false;
+    "media.ffmpeg.vaapi.enabled" = true;
+    "zen.theme.content-element-separation" = 0;
+  };
+
+  zen = pkgs.wrapFirefox zenPackages.zen-browser-unwrapped {
+    pname = "zen-browser";
+    extraPrefs = lib.concatLines (
+      lib.mapAttrsToList (
+        name: value: "defaultPref(${builtins.toJSON name}, ${builtins.toJSON value});"
+      )
+      prefs
+    );
+    extraPolicies = {
       DisableTelemetry = true;
       DisableFirefoxStudies = true;
       DisablePocket = true;
       DontCheckDefaultBrowser = true;
-
       ExtensionSettings = {
         "uBlock0@raymondhill.net" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
@@ -20,26 +43,13 @@
           private_browsing = true;
         };
       };
-
-      Preferences = lib.mapAttrs (_: value: {
-        inherit value;
-        Status = "default";
-      }) {
-        "extensions.autoDisableScopes" = 0;
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-        "browser.aboutConfig.showWarning" = false;
-        "browser.shell.checkDefaultBrowser" = false;
-        "browser.newtabpage.activity-stream.showSponsored" = false;
-        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-        "browser.urlbar.suggest.quicksuggest.sponsored" = false;
-        "toolkit.telemetry.enabled" = false;
-        "toolkit.telemetry.unified" = false;
-        "media.ffmpeg.vaapi.enabled" = true;
+      SearchEngines = {
+        Default = "DuckDuckGo";
       };
     };
   };
 in {
   config = lib.mkIf config.myModules.programs.zen.enable {
-    home.packages = [zen];
+    environment.systemPackages = [zen];
   };
 }
