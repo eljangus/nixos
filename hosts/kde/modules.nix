e31e45dbdccbe9d6859824a7abd37fbe531025a3
Elias Schlosser <eljangus@gmail.com>
2026-08-26 02:07:09 +0200

    using bunny cursor on plasma now too!

--- hosts/kde/modules.nix
+++ hosts/kde/modules.nix
@@ -1,5 +1,13 @@
-{...}: {
+{config, ...}: let
+  inherit (config.myModules) user;
+  myHome = {
+    theming = {
+      cursor.enable = true;
+    };
+  };
+in {
   myModules = {
     desktop = "plasma";
   };
+  home-manager.users.${user}.myModules.home-manager = myHome;
 }
